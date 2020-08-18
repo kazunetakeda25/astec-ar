709f327699bdaeff9d2eaeee840d83c3c573b709
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2020-08-18 16:03:39 -0400

    2

---
+++ Assets/DriveMaterialOffset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveMaterialOffset : MonoBehaviour
+{
+    public Material material;
+    public float currenY,speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenY += Time.deltaTime;
+        material.SetTextureOffset("_MainTex", new Vector2(0f, currenY * speed)); // _MainTex for Standard shader and for LWRP use "_BaseMap"
+    }
+
+    void OnEnable()
+    {
+        currenY = 0f;
+    }
+}
