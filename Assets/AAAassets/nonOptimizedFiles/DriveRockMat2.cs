b79717ab8379b1ba69b61761800cba6d18c4beac
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2020-08-18 17:33:36 -0400

    commit

---
+++ Assets/AAAassets/nonOptimizedFiles/DriveRockMat2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveRockMat2 : MonoBehaviour
+{
+    public Material material;
+    public float currenX,speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenX += Time.deltaTime;
+        material.SetTextureOffset("_MainTex", new Vector2( currenX * speed,0f)); // _MainTex for Standard shader and for LWRP use "_BaseMap" //BaseColorMap
+    }
+
+    void OnEnable()
+    {
+        currenX = 0f;
+    }
+}
